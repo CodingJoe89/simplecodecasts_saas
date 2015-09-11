22019020f200ffb33f093a34cb2c13cc278ea7b8
Joe Borromeo <joe.jamesborromeo@gmail.com>
2015-09-11 22:45:23 +0000

    added stripe js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    Stripe.setPubishableDey($('metal[name="stripe-key"]').attr('content'));
+    // watch for a form submission
+    $("#form-submit-btn").click(function(event) {
+        event.preventDefault();
+        $('input[type=submit]').prop('disabled', true);
+        var error = false;
+        var ccNum = $('#card_number').val(),
+            cvNum = $('#card_year').val(),
+            expMonth = $('#card_month').val(),
+            expYear = $('#card_year').val();
+            
+        if (!error) {
+            // get the Stipe token:
+            Stripe.creatToken({
+                number: ccNum,
+                cvc: cvcNum,
+                exp_month: expMonth,
+                exp_year: expYear
+            }, stripeResponseHandler);
+        }
+        return false;
+    }); // form submission
+    
+    function stripeResponseHandler(status, response) {
+        // Get a reference to the form:
+        var f = $("#new_user");
+        
+        // Get teh token form the response:
+        var token = response.id;
+        
+        // Add the token to the form:
+        f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+        
+        //Submit the form:
+        f.get(0).submit();
+    }
+});
